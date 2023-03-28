559c8e16a31e74107bdcecb98957b09552e434f7
Franco Yacaruso <francoyacaa@gmail.com>
2023-03-28 20:44:37 -0300

    agregando sliders

---
+++ main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 2,
+    slidesPerView: 4,
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  
