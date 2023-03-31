196c5c8d610b9e064bbf6350b79b89d9b2e607a2
Franco Yacaruso <francoyacaa@gmail.com>
2023-03-31 14:39:01 -0300

    haciendo slide responsive

--- main.js
+++ main.js
@@ -6,6 +6,21 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      200: {
+        slidesPerView: 1
+      },
+      500: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
   });
+
 
   
